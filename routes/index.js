5070275af60f1fbccdfd0666856e52d78cbaf110
trungdinhle <33343210+trungdinhle@users.noreply.github.com>
2019-04-20 22:25:40 +0700

    view divided

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var app_name = 'EleFind - Mua sắm điện thoại, máy tính và các thiết bị điện tử'
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: app_name });
 });
 
 module.exports = router;
