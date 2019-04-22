8fa340c900ba453664f0b5913473f7169ea0f7a8
trungdinhle <33343210+trungdinhle@users.noreply.github.com>
2019-04-22 09:26:19 +0700

    signup, sigin, update account info : done

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,23 @@ var app_name = 'EleFind - Mua sắm điện thoại, máy tính và các thiết
 router.get('/', function(req, res, next) {
   res.render('index', { title: app_name });
 });
+router.get('/xem-chi-tiet/', function(req, res, next) {
+  res.render('xem-chi-tiet', { title: app_name });
+});
 
+
+router.get('/dang-ky/', function(req, res, next) {
+  res.render('signup', { title:'EleFind - Đăng ký tài khoản' });
+});
+
+router.get('/dang-nhap/', function(req, res, next) {
+  res.render('signin', { title:'EleFind - Đăng ký tài khoản' ,customStyleSheet:'stylesheets/signin.css'});
+});
+
+router.get('/cap-nhat-tai-khoan/', function(req, res, next) {
+  res.render('updateinfo', { title:'EleFind - Cập nhật tài khoản' });
+});
+router.get('/cntk/', function(req, res, next) {
+  res.render('updateinfo', { title:'EleFind - Cập nhật tài khoản' });
+});
 module.exports = router;
